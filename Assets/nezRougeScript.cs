fc04357f388e7769eb473a69b7bcb86f7a3da5a1
LeoLecLeo <leolecuyer@ik.me>
2024-01-27 18:39:32 +0100

    code continue

---
+++ Assets/nezRougeScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nezRougeScript : MonoBehaviour
+{
+    CapsuleCollider2D capsuleNez;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public CapsuleCollider2D getCapsuleN(){
+
+        return this.capsuleNez;
+
+    }
+}
---
+++ Assets/scriptMask.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptMask : MonoBehaviour
+{
+
+    CapsuleCollider2D capsuleMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public CapsuleCollider2D GetCapsuleMask(){
+
+        return this.capsuleMask;
+
+    }
+
+}
