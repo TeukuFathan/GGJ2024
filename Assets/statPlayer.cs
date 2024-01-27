6f05b13f4d2ab1e837f7897b12f51dfee5fe5ef4
LeoLecLeo <leolecuyer@ik.me>
2024-01-27 19:18:42 +0100

    anim Mono + codeItems

--- Assets/statPlayer.cs
+++ Assets/statPlayer.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class statPlayer : MonoBehaviour
 {
     public float barreRire;
     public bool isDead;
+    public CapsuleCollider2D playerCapsule;
+    public scriptMask maskItem;
+
+    public int[] items = new int[5];
 
     // Start is called before the first frame update
     void Start()
     {
         barreRire = 0;
         isDead = false;
+        items[0] = 0;
+        items[1] = 0;
+        items[2] = 0;
+        items[3] = 0;
+        items[4] = 0;
     }
 
     void Update()
@@ -19,6 +29,12 @@ public class statPlayer : MonoBehaviour
         if(barreRire >= 100){
 
             this.isDead = true;
+
+        }
+
+        if(this.maskItem.GetCapsuleMask().IsTouching(this.playerCapsule)){
+
+            items[0] = 1;
 
         }
     }
