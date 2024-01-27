91cc9922bb590075c92d2799e888d836f8fee5d2
FERDIANSYAH Teuku <ferdiant@3il.fr>
2024-01-27 05:34:48 +0100

    recouvrement

--- Assets/tickleBotScript.cs
+++ Assets/tickleBotScript.cs
@@ -1,10 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.Collections;
-using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
 
 public class tickleBotScript : MonoBehaviour
