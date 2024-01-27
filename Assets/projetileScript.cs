43ec4f7ae19766a6728792d859e71421ff3ba6a3
FERDIANSYAH Teuku <ferdiant@3il.fr>
2024-01-27 04:53:43 +0100

    test

--- Assets/projetileScript.cs
+++ Assets/projetileScript.cs
@@ -12,7 +12,7 @@ public class projetileScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // rien
     }
 
     // Update is called once per frame
